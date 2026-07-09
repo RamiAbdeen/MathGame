9b04bf85740d930a35e0a2e4544efdfad618ccaf
Rami <rami01abdeen@gmail.com>
2026-07-09 10:55:06 +0300

    Add ReadNumberOfRounds function

--- MathGame/MathGame.cpp
+++ MathGame/MathGame.cpp
@@ -2,8 +2,21 @@
 
 using namespace std;
 
+short ReadNumberOfRounds()
+{
+	short NumberOfRounds;
+
+	do
+	{
+		cout << "How many rounds do you want to play? ";
+		cin >> NumberOfRounds;
+	} while (NumberOfRounds < 0);
+
+	return NumberOfRounds;
+}
+
 int main()
 {
-
+	
 	return 0;
 }
